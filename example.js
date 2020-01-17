4c43b0d2e612396c5b4e54a2a5fbe0385889ea82
khishigbayar <khishigbayar.edu@gmail.com>
2020-01-17 11:37:28 +0800

    Beldets file uudaa tataj avlaa. Java scriptiig ekhnees ni duustal hicheel 68

---
+++ example.js
@@ -0,0 +1,10 @@
+// var html = "<p>саин уу</p><P>саин уу</P><p>саин уу</p>";
+// html = html.replace(/p/gi, "strong");
+// console.log(html);
+
+var incDiv = document.querySelector(".income");
+incDiv.insertAdjacentHTML("beforeend", "Цалин : 2500000");
+
+incDiv.insertAdjacentHTML("beforeend", "<br>Сугалаа : 15000000");
+
+incDiv.insertAdjacentHTML("beforeend", "<br>Сугалаа : 15000000");
